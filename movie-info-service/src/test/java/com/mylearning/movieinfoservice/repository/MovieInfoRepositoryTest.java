ecccea51e7cc93e1db933123660b3b3be3d65c6f
prashant <prashanttiwary10@gmail.com>
2025-07-30 16:18:29 +0530

    mongodb test

--- movie-info-service/src/test/java/com/mylearning/movieinfoservice/repository/MovieInfoRepositoryTest.java
+++ movie-info-service/src/test/java/com/mylearning/movieinfoservice/repository/MovieInfoRepositoryTest.java
@@ -2,7 +2,6 @@ package com.mylearning.movieinfoservice.repository;
 
 import com.mylearning.movieinfoservice.model.MovieInfo;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
